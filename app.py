de49d9662988018eaf0b57d2dfcacdf74a2b5e29
Boateng Jephthah Agyenim <Boatengjephthah@gmail.com>
2022-06-03 09:07:00 +0000

    .

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ site_list = ["facebook.com", "youtube.com", "gmail.com", "freecoursesite.com", "
 
 
 while True:
-    if dt(dt.now().year, dt.now().month, dt.now().day, 9) < dt.now() < dt(dt.now().year, dt.now().month, dt.now().day, 16):
+    if dt(dt.now().year, dt.now().month, dt.now().day, 9, 30) < dt.now() < dt(dt.now().year, dt.now().month, dt.now().day, 16):
         print("Working hours.....")
         with open(host_temp, "r+") as file:
             content = file.read()
@@ -26,9 +26,11 @@ while True:
     else:
         with open(host_temp, "r+") as file:
             contents = file.readlines()
+            file.seek(0)
             for lines in contents:
                 if not any(website in lines for website in site_list):
                     file.write(lines)
+            file.truncate()
 
         print("Time to sleep!")
 
