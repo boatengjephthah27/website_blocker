ff40ace31d65efcf799a91270c4c080fd3760f66
Boateng Jephthah Agyenim <Boatengjephthah@gmail.com>
2022-06-03 10:48:00 +0000

    .

--- README.md
+++ README.md
@@ -1,5 +1,65 @@
+# ABOUT
+    This is an app that blocks a user from accessing some websites termed distracting during working or studying hours.
+
+
+### LIBRAIRIES USED
+    - Datetime
+        This is a module for providing classes to work with time and dates.
+        https://docs.python.org/3/library/datetime.html
+
+    - Time
+        This is a module for providing time related functions
+        https://docs.python.org/3/library/time.html
+
+
+### HOW TO SET IT UP AND RUN
+
+##### Mac/Linux Users
+    To run it as a system file. (from the /etc/hosts file) 
+
+    1.  To navigate to the file and view what is inside use
+        [sudo nano /etc/hosts]
+
+    2.  You need to provide administrative rights to your terminal
+        and you can do so by using the command (<sudo> before calling the program)
+        [sudo python3 blockapp.py] 
+
+    3.  Head to the crontab by typing the command
+        [sudo crontab -e]
+
+    4.  Navigate to the end of the file and add the command
+        [@reboot python3 <filepath>]
+        Which in my case is
+        [@reboot python3 ]
+
+
+
+
+##### Windows Users
+
+
+
+
+
+
 MAc users
 
 sudo nano /etc/hosts
 sudo crontab -e
 @reboot python3 -filepath
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+## Author
+Boateng Jephthah Agyenim
--- blockapp.py
+++ blockapp.py
@@ -1,36 +1,45 @@
-import time as t
+# Importing modules to be used
+import time as t 
 from datetime import datetime as dt    
 
 
 # Creating the necessary instances for the various path and https.
-host_temp = "hosts"
-host_path = "\etc\hosts"
-redirect = "127.0.0.1"
+host_temp = "hosts" # temp file to check if program works
+host_path = "\etc\hosts"  # True path to run the program
+redirect = "127.0.0.1"   # site to redirect the websites to if searched
 site_list = ["facebook.com", "youtube.com", "gmail.com", "freecoursesite.com", "www.facebook.com", "www.youtube.com", "www.gmail.com", "www.freecoursesite.com",
-"https://mail.google.com/mail/u/0/#inbox"]
+"https://mail.google.com/mail/u/0/#inbox", "www.twitter.com", "twitter.com", "netflix.com", "www.netflix.com", "www.yahoo.com", "yahoo.com"]   # lists of websites to block
 
-
+# Creating a loop to run the neccesary commands
 while True:
+    # calling a time range for working hours.
     if dt(dt.now().year, dt.now().month, dt.now().day, 9, 30) < dt.now() < dt(dt.now().year, dt.now().month, dt.now().day, 16):
         print("Working hours.....")
+
+        # opening the file from the file path, reading and performing operations on it
         with open(host_temp, "r+") as file:
             content = file.read()
+
+            # reading through the lines and appending the sites that are not in the file
             for websites in site_list:
                 if websites in content:
                     pass
+
                 else:
                     file.write(redirect + " " + websites + "\n")
 
         
 
-    else:
+    else:  # Calls to time outside the working hours
         with open(host_temp, "r+") as file:
-            contents = file.readlines()
-            file.seek(0)
+            contents = file.readlines() # Reading the lines in the file and not word by word
+            file.seek(0) # Places the cursor back to the start of the file
+
+            # 
             for lines in contents:
                 if not any(website in lines for website in site_list):
                     file.write(lines)
-            file.truncate()
+            file.truncate()  # Deletes all that prints after the first print
 
         print("Time to sleep!")
 
